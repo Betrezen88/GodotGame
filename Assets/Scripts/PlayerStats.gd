d786bf63e09297dcb1e22e7898f8b53210b11b40
Betrezen88 <wyszyn2000@gmail.com>
2023-02-26 17:26:50 +0100

    Added new player stats.

--- Assets/Scripts/PlayerStats.gd
+++ Assets/Scripts/PlayerStats.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 export var speed : float
-export var health : int
-export var stamina : int
+export var health : float
+export var stamina : float
+export var stamina_restore_value : float
+export var stamina_restore_interval: float
 export var power : int
