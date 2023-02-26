67e0fef58823373c33d41fd96653df18e2c00dd7
Betrezen88 <wyszyn2000@gmail.com>
2023-02-26 17:25:15 +0100

    Reworked receiving damage logic on HurtBox.

--- Assets/Scripts/HurtBox.gd
+++ Assets/Scripts/HurtBox.gd
@@ -1,22 +1,14 @@
 class_name HurtBox
 extends Area2D
 
+signal damage_received(value)
+
 func _init() ->void:
 	collision_layer = 0
 	collision_mask = 2
 
 func _ready() ->void:
-	print("HurtBox owner: ", owner)
 	connect("area_entered", self, "_on_area_entered")
 
 func _on_area_entered(hitbox: HitBox) -> void:
-	print("_on_area_entered")
-	if hitbox == null:
-		return
-	
-	print(owner)
-	
-	if owner.has_method("take_damage"):
-		owner.take_damage()
-	else:
-		print ("owner does not have take_damage method")
+	emit_signal("damage_received", 10)
