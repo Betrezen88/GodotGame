8a3641e025da4b1eb623f4ce8e93bc57d8e2cba6
Betrezen88 <wyszyn2000@gmail.com>
2023-04-15 00:16:20 +0200

    Reworked Dummy component.

--- Assets/Scripts/Character/Dummy.gd
+++ Assets/Scripts/Character/Dummy.gd
@@ -12,8 +12,7 @@ onready var frozenEffect = get_node(frozen_effect_path)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health.connect("value_changed", healthBar, "set_value")
-	healthRestore.connect("restore", health, "increase")
-	hurtBox.connect("damage_received", health, "deacrease")
+	hurtBox.connect("damage_received", health, "decrease")
 	hurtBox.connect("damage_received", frozenEffect, "add_value")
 	
 	healthBar.set_max_value(health.max_value)
