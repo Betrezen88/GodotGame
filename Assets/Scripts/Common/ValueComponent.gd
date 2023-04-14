522116d2a0df17796b64a9ff9ad777ce69428dbd
Betrezen88 <wyszyn2000@gmail.com>
2023-04-15 00:15:47 +0200

    Reworked ValueComponent script.

--- Assets/Scripts/Common/ValueComponent.gd
+++ Assets/Scripts/Common/ValueComponent.gd
@@ -1,25 +1,32 @@
 extends Node2D
 
 export var max_value : float
+export var min_value : float = 0
 export var current : float
 
-signal value_is_zero()
+signal value_is_min()
+signal value_is_max()
 signal value_changed(value)
 
 func _ready():
 	connect("value_changed", self, "on_value_changed")
 
-func increase(value) -> void:
-	current = clamp(current+value, 0, max_value)
+func increase(value: float) -> void:
+	current = clamp(current+value, min_value, max_value)
 	emit_signal("value_changed", current)
 
-func deacrease(value) -> void:
-	current = clamp(current-value, 0, max_value)
+func decrease(value: float) -> void:
+	current = clamp(current-value, min_value, max_value)
 	emit_signal("value_changed", current)
 
-func is_full() -> bool:
+func is_max() -> bool:
 	return current == max_value
+	
+func is_min() -> bool:
+	return current == min_value
 
 func on_value_changed(value) -> void:
-	if value <= 0:
-		emit_signal("value_is_zero")
+	if value == min_value:
+		emit_signal("value_is_min")
+	if value == max_value:
+		emit_signal("value_is_max")
